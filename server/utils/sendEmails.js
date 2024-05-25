e3554cad93a5796551600d64eea8d9c54f9aefbd
utkarsh chaturvedi <utkarshchaturvedi890@gmail.com>
2024-05-25 15:16:42 +0530

    User(Admin,student):Authentication and Authorization, subscription/unsubscribe , payment gateway (razorpay)

--- server/utils/sendEmails.js
+++ server/utils/sendEmails.js
@@ -1,23 +1,25 @@
 import nodemailer from "nodemailer";
+import dotenv from "dotenv";
 
 // async..await is not allowed in global scope, must use a wrapper
 const sendEmail = async function (email, subject, message) {
   // create reusable transporter object using the default SMTP transport
   let transporter = nodemailer.createTransport({
-    host: "smtp.ethereal.email",
+    host: process.env.SMTP_HOST ,
     port: 587,
     secure: false, // Use `true` for port 465, `false` for all other ports
     auth: {
-      user: "maddison53@ethereal.email",
-      pass: "jn7jnAPss4f63QBp6D",
+      user: process.env.SMTP_USERNAME,
+      pass:  process.env.SMTP_PASSWORD,
     },
   });
 
   // send mail with defined transport object
   await transporter.sendMail({
-    from: '"Maddison Foo Koch 👻" <maddison53@ethereal.email>', // sender address
-    to: "bar@example.com, baz@example.com", // list of receivers
-    subject: "Hello ✔", // Subject line
+    from: process.env.SMTP_FROM_EMAIL, // sender address
+    to: 'utkarshchaturvedi890@gmail.com', // list of receivers
+    //to get the mail from user so use 'req.body.email'
+    subject: "Subject of the gmail", // Subject line
 
     html: "<b>Hello me?</b>", // html body
   });
